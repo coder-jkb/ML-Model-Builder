6de77edfb469961f596e71dcf6c60c55e8ffd12f
coder-jkb <jkb17codes@gmail.com>
2022-09-12 00:20:50 +0530

    1. Added logo, login btn and signup btn

--- modelbuilder-frontend/pages/components/Header.js
+++ modelbuilder-frontend/pages/components/Header.js
@@ -5,19 +5,22 @@ const Header = () => {
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-purple-900 mb-4 md:mb-0">
-          <MdOutlineAutoGraph className="text-3xl"/>
-          <span className="ml-3 text-xl">ModelUp</span>
+          {/* <MdOutlineAutoGraph className="text-3xl"/> */}
+          <img src="/ModelUp_Logo.png" class="w-[7rem]" alt="logo" />
+          {/* <span className="ml-3 text-xl">ModelUp</span> */}
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">First Link</a>
-          <a className="mr-5 hover:text-gray-900">Second Link</a>
-          <a className="mr-5 hover:text-gray-900">Third Link</a>
-          <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+          <a className="py-1 px-3 rounded-md hover:text-purple-600 hover:bg-purple-100">First Link</a>
+          <a className="py-1 px-3 rounded-md hover:text-purple-600 hover:bg-purple-100">Second Link</a>
+          <a className="py-1 px-3 rounded-md hover:text-purple-600 hover:bg-purple-100">Third Link</a>
+          <a className="py-1 px-3 mr-3 rounded-md hover:text-purple-600 hover:bg-purple-100">Fourth Link</a>
         </nav>
-        <button className="inline-flex items-center bg-purple-100 border-0 py-1 px-3 focus:outline-none hover:bg-purple-300 rounded text-base mt-4 md:mt-0">Button
-          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <button className="inline-flex items-center mr-2 bg-transparent text-purple-600 border-2 border-purple-600 py-1 px-3 focus:outline-none hover:bg-purple-600 rounded hover:text-white mt-4 md:mt-0">Login
+        </button>
+        <button className="inline-flex items-center bg-transparent text-purple-600 border-2 border-purple-600 py-1 px-3 focus:outline-none hover:bg-purple-600 rounded hover:text-white mt-4 md:mt-0">Sign up
+          {/* <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
+          </svg> */}
         </button>
       </div>
     </header>
