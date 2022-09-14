e3e53628d0e6b2925c2be4da6d93e59972f27b6e
coder-jkb <jkb17codes@gmail.com>
2022-09-14 15:21:48 +0530

    added test view in django

--- modelbuilder-backend/app/urls.py
+++ modelbuilder-backend/app/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from .views import index
+from django.urls import path, include
+from .views import index, test
 urlpatterns = [
     path("", index, name="index"),
+    path("test/", test, name="test")
 ]
--- modelbuilder-backend/app/views.py
+++ modelbuilder-backend/app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django_nextjs.render import render_nextjs_page_sync
 
 def index(request):
     return render_nextjs_page_sync(request)
+
+def test(request):
+    return HttpResponse("<h1>test</h1>")
